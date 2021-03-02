88a3be66860bb595b30e994b33490dcb41578db6
PanicProtocol <79756701+PanicProtocol@users.noreply.github.com>
2021-03-02 15:14:54 +0530

    listed requirements

--- main.py
+++ main.py
@@ -7,5 +7,27 @@ db = mydb.connect(host="localhost",
 
 
 mycursor = db.cursor()
-#c_id, c_name, c_number,product_id,product_name,product_price
-mycursor.execute()
+
+'notes'
+
+#check if tables exist before (login table and sales table)
+#if table does not exist create table
+#login table requires name,password
+#sales table requires name,productname,price,phonenumber,address
+
+'''both tables should be able to have values inserted and values retrieved 
+
+i dont remember python + mysql so correct whatever is wrong, but you get it right?
+
+def usersignup(uname,passcode):
+	mycursor.execute(f"INSERT INTO TABLE logins VALUES({uname},{passcode})")
+
+def loginpull():
+	mycursor.execute("select * from logins ..........") #rest i forgot python+msql but it should be available as a list/tuple
+
+def order(uname,pname,price,pnum,address):
+	mycursor.execute(f"INSERT INTO TABLE logins VALUES({uname},{passcode})")
+
+def searchdat(option):
+	displaying things, ill type later
+'''
