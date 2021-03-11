4182a3e73324419f3cc54f58bd4f63e81de972cc
Risshikeswaraan <62908447+caramelo-carnicero@users.noreply.github.com>
2021-03-11 15:09:57 +0530

    Update fa_gui.py

--- GUI/fa_gui.py
+++ GUI/fa_gui.py
@@ -22,7 +22,7 @@ uname=""        #global variable for username
 
 pid=""          #global variable for product ID
 
-prodlist=[("ALPHA","7,499"),("TAPTIME","10,000"),("JHOSHLET","1"),("PORTCOMP","69"),("EECHO BOX","2,500"),("GLAREELS","999999")] #names of products
+prodlist=[("ALPHA","7,499"),("TAPTIME","10,000"),("JHOSHLET","10000"),("PORTCOMP","65000"),("EECHO BOX","2,500"),("GLAREELS","7599")] #names of products
 
 
 f=open("desc.txt",'r')
@@ -160,11 +160,11 @@ Rishikeswaran V""")
         startbutt=tk.Button(frame,text="Log in",font="Roboto 20",bg=buttoncol,fg="black",command=lambda:verify((uentry.get()).strip(),(pentry.get()).strip()))
         startbutt.place(relx=0.55,rely=0.75,relwidth=0.3,relheight=0.1) #places button in the window
 
-        signbutt=tk.Button(frame,text="sign up",font="Roboto 20",bg=buttoncol,fg="black",command=lambda:signup((uentry.get()).strip(),(pentry.get()).strip()))
+        signbutt=tk.Button(frame,text="Sign-up",font="Roboto 20",bg=buttoncol,fg="black",command=lambda:signup((uentry.get()).strip(),(pentry.get()).strip()))
         signbutt.place(relx=0.15,rely=0.75,relwidth=0.3,relheight=0.1) #places button in the window
 
-        helpbutt=tk.Button(frame,text="help",font="Roboto 20",bg=buttoncol,fg="black",command=lambda:helpinfo())
-        helpbutt.place(relx=0.004,rely=0.11,relwidth=0.15,relheight=0.05) #places button in the window
+        helpbutt=tk.Button(frame,text="Help",font="Roboto 15",bg=buttoncol,fg="black",command=lambda:helpinfo())
+        helpbutt.place(relx=0.004,rely=0.11,relwidth=0.10,relheight=0.04) #places button in the window
 
 
 
@@ -335,8 +335,8 @@ class itempg():
         label.place(relx=0,rely=0,relwidth=1,relheight=0.2)
 
 
-        desclabel=tk.Label(frame,text=f"{proddesc}",justify='left',font="Roboto 15",bg="black",fg="white")                    
-        desclabel.place(relx=0.6,rely=0.37,relwidth=0.3,relheight=0.4)
+        desclabel=tk.Label(frame,text=f"{proddesc}",justify='left',font="Roboto 15",wraplength=500,bg="black",fg="white")                    
+        desclabel.place(relx=0.5,rely=0.31,relwidth=0.5,relheight=0.45)
 
         global pimage   #prevents image from being garbage collected
 
@@ -397,13 +397,13 @@ class buypg():
             label=tk.Label(frame,text=f"Enter your details {uname}",font="Roboto 40",bg=labelcol,fg=ltextcol)                    
             label.place(relx=0,rely=0,relwidth=1,relheight=0.2)
 
-            alabel=tk.Label(frame,text=f"Enter your address",font="Roboto 10",bg=mainbg)                    
+            alabel=tk.Label(frame,text=f"Enter your address",font="Roboto 10",bg=labelcol,fg="white")                    
             alabel.place(relx=0,rely=0.3,relwidth=1,relheight=0.05)
 
             adentry=tk.Entry(frame,bg=buttoncol,fg="black",font="Roboto 15") #address entry
             adentry.place(relx=0.25,rely=0.4,relwidth=0.5,relheight=0.075)
 
-            plabel=tk.Label(frame,text=f"Enter your phone number",font="Roboto 10",bg=mainbg)                    
+            plabel=tk.Label(frame,text=f"Enter your phone number",font="Roboto 10",bg=labelcol,fg="white")                    
             plabel.place(relx=0,rely=0.5,relwidth=1,relheight=0.05)
 
             pentry=tk.Entry(frame,bg=buttoncol,fg="black",font="Roboto 15") #phone num entry
